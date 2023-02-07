251e87dffa8863c2efa10959c9a3c1bad2b8bec4
NVTFT <NVTFT@DESKTOP-S1EOF8Q>
2023-02-07 16:07:36 +0700

    create folder

---
+++ app/src/main/java/com/example/myapplication/data/model/Category.java
@@ -0,0 +1,42 @@
+package com.example.myapplication.data.model;
+
+import java.util.List;
+
+public class Category {
+    private int cate_id;
+    private String cate_name;
+    private List<Category> child_cate;
+
+    public Category() {
+    }
+
+    public Category(int cate_id, String cate_name, List<Category> child_cate) {
+        this.cate_id = cate_id;
+        this.cate_name = cate_name;
+        this.child_cate = child_cate;
+    }
+
+    public int getCate_id() {
+        return cate_id;
+    }
+
+    public void setCate_id(int cate_id) {
+        this.cate_id = cate_id;
+    }
+
+    public String getCate_name() {
+        return cate_name;
+    }
+
+    public void setCate_name(String cate_name) {
+        this.cate_name = cate_name;
+    }
+
+    public List<Category> getChild_cate() {
+        return child_cate;
+    }
+
+    public void setChild_cate(List<Category> child_cate) {
+        this.child_cate = child_cate;
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/data/model/Expense.java
@@ -0,0 +1,60 @@
+package com.example.myapplication.data.model;
+
+public class Expense {
+    private int expense_id;
+    private String expense_name;
+    private int cate_id;
+    private String description;
+    private Float amount;
+
+    public Expense() {
+    }
+
+    public Expense(int expense_id, String expense_name, int cate_id, String description, Float amount) {
+        this.expense_id = expense_id;
+        this.expense_name = expense_name;
+        this.cate_id = cate_id;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public int getExpense_id() {
+        return expense_id;
+    }
+
+    public void setExpense_id(int expense_id) {
+        this.expense_id = expense_id;
+    }
+
+    public String getExpense_name() {
+        return expense_name;
+    }
+
+    public void setExpense_name(String expense_name) {
+        this.expense_name = expense_name;
+    }
+
+    public int getCate_id() {
+        return cate_id;
+    }
+
+    public void setCate_id(int cate_id) {
+        this.cate_id = cate_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Float amount) {
+        this.amount = amount;
+    }
+}
